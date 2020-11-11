567a86f88ac42bb12ed2d14e374047ee1acca681
LilSalty-cpu <72315069+LilSalty-cpu@users.noreply.github.com>
2020-11-11 18:06:07 +0100

    Update purge.js

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: "purge",
     category: "moderation",
-    description: "Deletes messages in a text channel or specified number of messages in a text channel.",
-    usage: "[COMMAND] OR [COMMAND] + [number of messages]",
+    description: "Supprime le nombre de message souhaitez",
+    usage: "[COMMANDE] OR [COMMANDE] + [nombre de message]",
     run: async (client, message, args) => {
-        message.channel.send(`Command under construction!`);
+        message.channel.send(`La commande est en construction ! Une MAJ sera bientot déployé !`);
 
     }
 }
